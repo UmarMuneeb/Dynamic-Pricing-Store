d19725ff1da1315df1677134b88a7036d913bb0c
UmarMuneeb <umarmuneeb451@gmail.com>
2026-01-22 16:04:10 +0500

    Product Controller Created

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   get '/health', to: 'health#index'
 
   # API routes will go here
+  # API routes
   namespace :api do
-    namespace :v1 do
-      # Future routes
-    end
+    resources :products, only: [:index]
   end
 end
